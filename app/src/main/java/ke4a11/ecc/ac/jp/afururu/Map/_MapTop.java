4bcad5b428ed68642025683fc7d309722a38afe1
kinoryo <kinosita.ryota@gmail.com>
2016-01-15 21:09:31 +0900

    訂正版　こっち入れないとエラーおきます ＊至急push求む

--- app/src/main/java/ke4a11/ecc/ac/jp/afururu/Map/_MapTop.java
+++ app/src/main/java/ke4a11/ecc/ac/jp/afururu/Map/_MapTop.java
@@ -22,7 +22,7 @@ public class _MapTop extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.activity_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map_top, container, false);
 
         return view;
     }
