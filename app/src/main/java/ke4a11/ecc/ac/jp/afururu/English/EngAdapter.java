baacd45b45174c4065a84fadefa61f5a4465420a
wcak <black.rabbit357@gmaiil.com>
2016-02-05 20:02:51 +0900

    完成！！！！！！！！！！！！

---
+++ app/src/main/java/ke4a11/ecc/ac/jp/afururu/English/EngAdapter.java
@@ -0,0 +1,103 @@
+package ke4a11.ecc.ac.jp.afururu.English;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import ke4a11.ecc.ac.jp.afururu.R;
+
+/**
+ * Created by it on 2016/01/28.
+ */
+public class EngAdapter extends BaseExpandableListAdapter {
+    private Context ctx;
+    private LinkedHashMap<String, List<String>> eng_category;
+    private List<String> eng_list;
+
+    public EngAdapter(Context ctx, LinkedHashMap<String, List<String>> eng_category, List<String> eng_list)
+    {
+        this.ctx = ctx;
+        this.eng_category = eng_category;
+        this.eng_list = eng_list;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return eng_list.size();
+    }
+
+    @Override
+    //カスタマイズ
+    public int getChildrenCount(int groupPosition) {
+        return eng_category.get(eng_list.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return eng_list.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int parent, int child) {
+        return eng_category.get(eng_list.get(parent)).get(child);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int parent, int child) {
+        return child;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int parnet, boolean isExpanded, View convertView, ViewGroup parentview) {
+
+        String group_title = (String) getGroup(parnet);
+        if(convertView == null){
+            LayoutInflater inflator = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflator.inflate(R.layout.parent_layout, parentview,false);
+        }
+        TextView parnet_textview = (TextView) convertView.findViewById(R.id.textView);
+        parnet_textview.setTypeface(null, Typeface.BOLD);
+        parnet_textview.setText(group_title);
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int parent, int child, boolean lastchild, View convertView, ViewGroup parentview) {
+
+        String child_title = (String) getChild(parent,child);
+        if(convertView == null)
+        {
+            LayoutInflater inflator = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflator.inflate(R.layout.child_layout, parentview,false);
+        }
+        TextView child_textview = (TextView) convertView.findViewById(R.id.textView2);
+        child_textview.setText(child_title);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition)
+    {
+        return true;
+    }
+}
--- app/src/main/java/ke4a11/ecc/ac/jp/afururu/English/_EnglishTop.java
+++ app/src/main/java/ke4a11/ecc/ac/jp/afururu/English/_EnglishTop.java
@@ -1,123 +1,136 @@
 package ke4a11.ecc.ac.jp.afururu.English;
 
-import java.util.Locale;
-import java.util.Queue;
 import android.content.Intent;
 import android.os.Bundle;
-//FragmentPagerAdapter で使うために supportlibrary の fragment を使用
-//
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.content.Context;
-import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 import ke4a11.ecc.ac.jp.afururu.R;
 
+
 public class _EnglishTop extends Fragment {
 
-    private TextToSpeech    tts;
-    private Button buttonSpeech;
-    private static View view;
+    // ボタンを管理する変数
 
-    @Override
+    private ImageButton enghotButton;
+    private ImageButton engresButton;
+    private ImageButton engairButton;
+    private ImageButton engschButton;
+    private ImageButton emeButton;
+    private ImageButton teiButton;
+    // private ImageButton 新しいボタン;
+
+    //HashMapのリストを出す
+    private LinkedHashMap<String, List<String>> eng_category;
+
+    @Override     // 必要な手続きを書いておく
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        if (view == null) {
-            view = inflater.inflate(R.layout.fragment_english_top, container, false);
-        }
-
-        //ボタン作成
-        ImageButton engresButton = (ImageButton)view.findViewById(R.id.engresButton);
-        ImageButton enghotButton = (ImageButton)view.findViewById(R.id.enghotButton);
-        ImageButton engairButton = (ImageButton)view.findViewById(R.id.engairButton);
-        ImageButton engschButton = (ImageButton)view.findViewById(R.id.engschButton);
-        ImageButton emeButton = (ImageButton)view.findViewById(R.id.emeButton);
-        ImageButton teiButton = (ImageButton)view.findViewById(R.id.teiButton);
-        //リスナの設定
-        engresButton.setOnClickListener(new ChangeView());
-        enghotButton.setOnClickListener(new ChangeView());
-        engairButton.setOnClickListener(new ChangeView());
-        engschButton.setOnClickListener(new ChangeView());
-        emeButton.setOnClickListener(new ChangeView());
-        teiButton.setOnClickListener(new ChangeView());
-
-        return view;
+        return inflater.inflate(R.layout.fragment_english_top, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
-    //リスナの内容
-    class ChangeView implements OnClickListener{
+        //ボタン作成
 
-        public void onClick(View v){
+        enghotButton = (ImageButton) getActivity().findViewById(R.id.enghotButton);
+        engresButton = (ImageButton) getActivity().findViewById(R.id.engresButton);
+        engairButton = (ImageButton) getActivity().findViewById(R.id.engairButton);
+        engschButton = (ImageButton) getActivity().findViewById(R.id.engschButton);
+        emeButton = (ImageButton) getActivity().findViewById(R.id.emeButton);
+        teiButton = (ImageButton) getActivity().findViewById(R.id.teiButton);
 
-            if(v == getView().findViewById(R.id.engresButton)){
-                //押したボタンに対応したフレーズを代入する
-                English_Textview_Activity.titleEngText = Eng_Text.TITLES_hotel;
-                English_Detail.detailEngText = Eng_Text.DIALOG_hotel;
+        //新しいボタン = (ImageButton) getActivity().findViewById(R.id.enghotButton);
 
+        //top画像
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[][] english;
+                LinkedHashMap<String, List<String>> japanese;
+
+                switch (v.getId()) {
+
+                    case R.id.enghotButton: // ホテル
+                        english = Eng_Text.DIALOG_hotel;
+                        japanese = Category.getHotelInfo();
+                        break;
+
+                    case R.id.engresButton: // レストラン
+                        english = Eng_Text.DIALOG_res;
+                        japanese = Category.getrestInfo();
+                        break;
+
+                    case R.id.engairButton: // 空港
+                        english = Eng_Text.DIALOG_airport;
+                        japanese = Category.getairInfo();
+                        break;
+
+                    case R.id.engschButton: // 学校
+                        english = Eng_Text.DIALOG_life;
+                        japanese = Category.getschoolInfo();
+                        break;
+
+                    case R.id.emeButton: // 緊急
+                        english = Eng_Text.DIALOG_emergency;
+                        japanese = Category.getemergencyInfo();
+                        break;
+
+                    case R.id.teiButton: // 丁寧
+                        english = Eng_Text.DIALOG_airport;
+                        japanese = Category.getairInfo();
+                        break;
+
+
+
+
+
+
+//                    case R.id.enghotButton: // 新しいシーン
+//                        english = Eng_Text.DIALOG_hotel;
+//                        japanese = Category.getHotelInfo();
+//                        break;
+                    default:
+                        english = Eng_Text.DIALOG_hotel;
+                        japanese = Category.getHotelInfo();
+                }
+
+                // 次の画面にデータを送る
+                ExpandableListViewActivity.english = english;
+                ExpandableListViewActivity.japanese = japanese;
+
+                // 次の画面に遷移する
                 Intent intent = getActivity().getIntent();
-                intent.setClass(getActivity(), English_Textview_Activity.class);
+                intent.setClass(getActivity(), ExpandableListViewActivity.class);
+
                 //これがないと初回起動時以外反応しない
                 //Activityが破棄されていないのでstartされない（たぶｎ）
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
-            }else if(v == getView().findViewById(R.id.enghotButton)){
-                English_Textview_Activity.titleEngText = Eng_Text.TITLES_airport;
-                English_Detail.detailEngText = Eng_Text.DIALOG_airport;
-
-                Intent intent = getActivity().getIntent();
-                intent.setClass(getActivity(), English_Textview_Activity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            }else if(v == getView().findViewById(R.id.engairButton)){
-                English_Textview_Activity.titleEngText = Eng_Text.TITLES_res;
-                English_Detail.detailEngText = Eng_Text.DIALOG_res;
-
-                Intent intent = getActivity().getIntent();
-                intent.setClass(getActivity(), English_Textview_Activity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            }else if(v == getView().findViewById(R.id.engschButton)){
-                English_Textview_Activity.titleEngText = Eng_Text.TITLES_life;
-                English_Detail.detailEngText = Eng_Text.DIALOG_life;
-
-                Intent intent = getActivity().getIntent();
-                intent.setClass(getActivity(), English_Textview_Activity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            }else if(v == getView().findViewById(R.id.emeButton)){
-                English_Textview_Activity.titleEngText = Eng_Text.TITLES_emergency;
-                English_Detail.detailEngText = Eng_Text.DIALOG_emergency;
-
-                Intent intent = getActivity().getIntent();
-                intent.setClass(getActivity(), English_Textview_Activity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            }else if(v == getView().findViewById(R.id.teiButton)){
-                English_Textview_Activity.titleEngText = Eng_Text.TITLES_life;
-                English_Detail.detailEngText = Eng_Text.DIALOG_life;
-
-                Intent intent = getActivity().getIntent();
-                intent.setClass(getActivity(), English_Textview_Activity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
             }
+        };
 
-        }
-
+        //リスナの設定
+        enghotButton.setOnClickListener(onClickListener);
+        engresButton.setOnClickListener(onClickListener);
+        engairButton.setOnClickListener(onClickListener);
+        engschButton.setOnClickListener(onClickListener);
+        emeButton.setOnClickListener(onClickListener);
+        teiButton.setOnClickListener(onClickListener);
     }
-
-
 }
+
+
+
+
